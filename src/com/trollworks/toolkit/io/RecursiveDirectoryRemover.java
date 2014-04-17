ab9dfdaa655d1ff3656acde7cbc4c166ed986200
Richard Wilkes <rich@trollworks.com>
2014-04-17 16:50:36 -0700

    Provides simple full directory removal.

---
+++ src/com/trollworks/toolkit/io/RecursiveDirectoryRemover.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 1998-2014 by Richard A. Wilkes. All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * version 2.0. If a copy of the MPL was not distributed with this file, You
+ * can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This Source Code Form is "Incompatible With Secondary Licenses", as defined
+ * by the Mozilla Public License, version 2.0.
+ */
+
+package com.trollworks.toolkit.io;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class RecursiveDirectoryRemover implements FileVisitor<Path> {
+	private Path	mPath;
+
+	public static final void remove(Path path, boolean includeRootDir) {
+		try {
+			Files.walkFileTree(path, new RecursiveDirectoryRemover(includeRootDir ? null : path));
+		} catch (IOException exception) {
+			Log.error(exception);
+		}
+	}
+
+	private RecursiveDirectoryRemover(Path exclude) {
+		mPath = exclude;
+	}
+
+	@Override
+	public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+		return FileVisitResult.CONTINUE;
+	}
+
+	@Override
+	public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+		Files.delete(file);
+		return FileVisitResult.CONTINUE;
+	}
+
+	@Override
+	public FileVisitResult visitFileFailed(Path file, IOException exception) throws IOException {
+		Log.error(exception);
+		return FileVisitResult.CONTINUE;
+	}
+
+	@Override
+	public FileVisitResult postVisitDirectory(Path dir, IOException exception) throws IOException {
+		if (exception != null) {
+			Log.error(exception);
+		}
+		if (!dir.equals(mPath)) {
+			Files.delete(dir);
+		}
+		return FileVisitResult.CONTINUE;
+	}
+}
