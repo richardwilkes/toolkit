b4f7265e09b46e261c20b49bb25fadb7c1bf8c0c
Richard Wilkes <rich@trollworks.com>
2014-04-17 17:00:03 -0700

    Added LRU caches.

---
+++ src/com/trollworks/toolkit/collections/BufferedImageLRUCache.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 1998-2014 by Richard A. Wilkes. All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * version 2.0. If a copy of the MPL was not distributed with this file, You
+ * can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This Source Code Form is "Incompatible With Secondary Licenses", as defined
+ * by the Mozilla Public License, version 2.0.
+ */
+
+package com.trollworks.toolkit.collections;
+
+import java.awt.image.BufferedImage;
+import java.util.Map;
+
+/**
+ * Provides a {@link LRUCache} for {@link BufferedImage}s, flushing their contents when removed from
+ * the cache.
+ */
+public class BufferedImageLRUCache<K> extends LRUCache<K, BufferedImage> {
+	/**
+	 * Creates a new {@link BufferedImageLRUCache}.
+	 *
+	 * @param maxEntries The maximum number of entries to be in the cache. Older entries are removed
+	 *            first.
+	 */
+	public BufferedImageLRUCache(int maxEntries) {
+		super(maxEntries);
+	}
+
+	@Override
+	protected boolean removeEldestEntry(Map.Entry<K, BufferedImage> eldest) {
+		if (super.removeEldestEntry(eldest)) {
+			eldest.getValue().flush();
+			return true;
+		}
+		return false;
+	}
+}
---
+++ src/com/trollworks/toolkit/collections/LRUCache.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 1998-2014 by Richard A. Wilkes. All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * version 2.0. If a copy of the MPL was not distributed with this file, You
+ * can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This Source Code Form is "Incompatible With Secondary Licenses", as defined
+ * by the Mozilla Public License, version 2.0.
+ */
+
+package com.trollworks.toolkit.collections;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * Implementation of a least recently used cache. As new entries are added older entries are
+ * removed. Calling get() or put() will refresh the entry. Calling containsKey() will not.
+ */
+public class LRUCache<K, V> extends LinkedHashMap<K, V> {
+	private final int	mMaxEntries;
+
+	/**
+	 * Creates a new {@link LRUCache}.
+	 *
+	 * @param maxEntries The maximum number of entries to be in the cache. Older entries are removed
+	 *            first.
+	 */
+	public LRUCache(int maxEntries) {
+		super(16, 0.75f, true);
+		mMaxEntries = maxEntries;
+	}
+
+	@Override
+	protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+		return size() > mMaxEntries;
+	}
+}
