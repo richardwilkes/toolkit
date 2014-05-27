5d1287b0f11e8fb312bf99b6fc60d1d4ea80663a
Richard Wilkes <rich@trollworks.com>
2014-05-26 18:48:50 -0700

    Better matching for the various OS's.

--- src/com/trollworks/toolkit/ui/widget/KeyStrokeDisplay.java
+++ src/com/trollworks/toolkit/ui/widget/KeyStrokeDisplay.java
@@ -13,6 +13,9 @@ package com.trollworks.toolkit.ui.widget;
 
 import static java.awt.event.KeyEvent.*;
 
+import com.trollworks.toolkit.ui.UIUtilities;
+import com.trollworks.toolkit.utility.Platform;
+
 import java.awt.Color;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
@@ -21,33 +24,14 @@ import java.awt.event.KeyListener;
 import javax.swing.JLabel;
 import javax.swing.KeyStroke;
 import javax.swing.SwingConstants;
+import javax.swing.UIManager;
 import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 
 /** Displays and captures keystrokes typed. */
 public class KeyStrokeDisplay extends JLabel implements KeyListener {
-	public static final String	LEFT_ARROW_SYMBOL		= "\u2190"; //$NON-NLS-1$
-	public static final String	UP_ARROW_SYMBOL			= "\u2191"; //$NON-NLS-1$
-	public static final String	RIGHT_ARROW_SYMBOL		= "\u2192"; //$NON-NLS-1$
-	public static final String	DOWN_ARROW_SYMBOL		= "\u2193"; //$NON-NLS-1$
-	public static final String	HOME_SYMBOL				= "\u2196"; //$NON-NLS-1$
-	public static final String	END_SYMBOL				= "\u2198"; //$NON-NLS-1$
-	public static final String	PAGE_UP_SYMBOL			= "\u21de"; //$NON-NLS-1$
-	public static final String	PAGE_DOWN_SYMBOL		= "\u21df"; //$NON-NLS-1$
-	public static final String	TAB_SYMBOL				= "\u21e5"; //$NON-NLS-1$
-	public static final String	SHIFT_SYMBOL			= "\u21e7"; //$NON-NLS-1$
-	public static final String	CAPSLOCK_SYMBOL			= "\u21ea"; //$NON-NLS-1$
-	public static final String	CONTROL_SYMBOL			= "\u2303"; //$NON-NLS-1$
-	public static final String	COMMAND_SYMBOL			= "\u2318"; //$NON-NLS-1$
-	public static final String	ENTER_SYMBOL			= "\u2324"; //$NON-NLS-1$
-	public static final String	OPTION_SYMBOL			= "\u2325"; //$NON-NLS-1$
-	public static final String	FORWARD_DELETE_SYMBOL	= "\u2326"; //$NON-NLS-1$
-	public static final String	CLEAR_SYMBOL			= "\u2327"; //$NON-NLS-1$
-	public static final String	DELETE_SYMBOL			= "\u232b"; //$NON-NLS-1$
-	public static final String	ESCAPE_SYMBOL			= "\u238b"; //$NON-NLS-1$
-	public static final String	RETURN_SYMBOL			= "\u23ce"; //$NON-NLS-1$
-	private KeyStroke			mKeyStroke;
+	private KeyStroke	mKeyStroke;
 
 	/**
 	 * Creates a new {@link KeyStrokeDisplay}.
@@ -55,13 +39,13 @@ public class KeyStrokeDisplay extends JLabel implements KeyListener {
 	 * @param ks The {@link KeyStroke} to start with.
 	 */
 	public KeyStrokeDisplay(KeyStroke ks) {
-		super("Command-Alt-Option-Control-Shift-Meta-Z", SwingConstants.CENTER); //$NON-NLS-1$
+		super(getKeyStrokeDisplay(KeyStroke.getKeyStroke('Z', InputEvent.META_MASK | InputEvent.ALT_MASK | InputEvent.CTRL_MASK | InputEvent.SHIFT_MASK)), SwingConstants.CENTER);
 		setOpaque(true);
 		setBackground(Color.WHITE);
 		setBorder(new CompoundBorder(LineBorder.createBlackLineBorder(), new EmptyBorder(2, 5, 2, 5)));
 		addKeyListener(this);
 		mKeyStroke = ks;
-		setPreferredSize(getPreferredSize());
+		UIUtilities.setOnlySize(this, getPreferredSize());
 		setText(getKeyStrokeDisplay(mKeyStroke));
 	}
 
@@ -99,105 +83,23 @@ public class KeyStrokeDisplay extends JLabel implements KeyListener {
 		if (ks != null) {
 			int modifiers = ks.getModifiers();
 			if (modifiers > 0) {
-				if ((modifiers & InputEvent.CTRL_MASK) != 0) {
-					buffer.append(CONTROL_SYMBOL);
-				}
-				if ((modifiers & InputEvent.ALT_MASK) != 0) {
-					buffer.append(OPTION_SYMBOL);
-				}
-				if ((modifiers & InputEvent.SHIFT_MASK) != 0) {
-					buffer.append(SHIFT_SYMBOL);
-				}
-				if ((modifiers & InputEvent.META_MASK) != 0) {
-					buffer.append(COMMAND_SYMBOL);
+				String modifierText = KeyEvent.getKeyModifiersText(modifiers);
+				if (Platform.isMacintosh()) {
+					buffer.append(modifierText.replaceAll("\\+", "")); //$NON-NLS-1$ //$NON-NLS-2$
+				} else {
+					buffer.append(modifierText);
+					String delimiter = UIManager.getString("MenuItem.acceleratorDelimiter"); //$NON-NLS-1$
+					if (delimiter == null) {
+						delimiter = "+"; //$NON-NLS-1$
+					}
+					buffer.append(delimiter);
 				}
 			}
 			int keyCode = ks.getKeyCode();
-			switch (keyCode) {
-				case 0:
-					buffer.append(ks.getKeyChar());
-					break;
-				case VK_LEFT:
-					buffer.append(LEFT_ARROW_SYMBOL);
-					break;
-				case VK_UP:
-					buffer.append(UP_ARROW_SYMBOL);
-					break;
-				case VK_RIGHT:
-					buffer.append(RIGHT_ARROW_SYMBOL);
-					break;
-				case VK_DOWN:
-					buffer.append(DOWN_ARROW_SYMBOL);
-					break;
-				case VK_HOME:
-					buffer.append(HOME_SYMBOL);
-					break;
-				case VK_END:
-					buffer.append(END_SYMBOL);
-					break;
-				case VK_PAGE_UP:
-					buffer.append(PAGE_UP_SYMBOL);
-					break;
-				case VK_PAGE_DOWN:
-					buffer.append(PAGE_DOWN_SYMBOL);
-					break;
-				case VK_TAB:
-					buffer.append(TAB_SYMBOL);
-					break;
-				case VK_CAPS_LOCK:
-					buffer.append(CAPSLOCK_SYMBOL);
-					break;
-				case VK_ENTER:
-					buffer.append(ENTER_SYMBOL);
-					break;
-				case VK_DELETE:
-					buffer.append(FORWARD_DELETE_SYMBOL);
-					break;
-				case VK_BACK_SPACE:
-					buffer.append(DELETE_SYMBOL);
-					break;
-				case VK_ESCAPE:
-					buffer.append(ESCAPE_SYMBOL);
-					break;
-				case VK_CLEAR:
-					buffer.append(CLEAR_SYMBOL);
-					break;
-				case VK_BACK_QUOTE:
-					buffer.append("`"); //$NON-NLS-1$
-					break;
-				case VK_MINUS:
-					buffer.append("-"); //$NON-NLS-1$
-					break;
-				case VK_EQUALS:
-					buffer.append("="); //$NON-NLS-1$
-					break;
-				case VK_OPEN_BRACKET:
-					buffer.append("["); //$NON-NLS-1$
-					break;
-				case VK_CLOSE_BRACKET:
-					buffer.append("]"); //$NON-NLS-1$
-					break;
-				case VK_BACK_SLASH:
-					buffer.append("\\"); //$NON-NLS-1$
-					break;
-				case VK_SEMICOLON:
-					buffer.append(";"); //$NON-NLS-1$
-					break;
-				case VK_QUOTE:
-					buffer.append("'"); //$NON-NLS-1$
-					break;
-				case VK_COMMA:
-					buffer.append(","); //$NON-NLS-1$
-					break;
-				case VK_PERIOD:
-					buffer.append("."); //$NON-NLS-1$
-					break;
-				case VK_SLASH:
-					buffer.append("/"); //$NON-NLS-1$
-					break;
-				default:
-					buffer.append(getKeyText(keyCode));
-					break;
+			if (keyCode != 0) {
+				buffer.append(KeyEvent.getKeyText(keyCode));
+			} else {
+				buffer.append(ks.getKeyChar());
 			}
 		}
 		return buffer.toString();
