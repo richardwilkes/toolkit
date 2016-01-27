6af4bf339ab784a63aee7f58aa5e7bccf7d56d4c
Richard Wilkes <rich@trollworks.com>
2016-01-27 12:14:04 -0800

    Initial checkin.

---
+++ src/com/trollworks/toolkit/utility/Buffers.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 1998-2016 by Richard A. Wilkes. All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * version 2.0. If a copy of the MPL was not distributed with this file, You
+ * can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This Source Code Form is "Incompatible With Secondary Licenses", as defined
+ * by the Mozilla Public License, version 2.0.
+ */
+
+package com.trollworks.toolkit.utility;
+
+import java.nio.ByteBuffer;
+
+public final class Buffers {
+	public final static byte[] toBytes(char[] data) {
+		ByteBuffer bytes = ByteBuffer.allocate(data.length * Character.BYTES);
+		bytes.asCharBuffer().put(data);
+		return bytes.array();
+	}
+
+	public final static byte[] toBytes(short[] data) {
+		ByteBuffer bytes = ByteBuffer.allocate(data.length * Short.BYTES);
+		bytes.asShortBuffer().put(data);
+		return bytes.array();
+	}
+
+	public final static byte[] toBytes(int[] data) {
+		ByteBuffer bytes = ByteBuffer.allocate(data.length * Integer.BYTES);
+		bytes.asIntBuffer().put(data);
+		return bytes.array();
+	}
+
+	public final static byte[] toBytes(long[] data) {
+		ByteBuffer bytes = ByteBuffer.allocate(data.length * Long.BYTES);
+		bytes.asLongBuffer().put(data);
+		return bytes.array();
+	}
+
+	public final static byte[] toBytes(float[] data) {
+		ByteBuffer bytes = ByteBuffer.allocate(data.length * Float.BYTES);
+		bytes.asFloatBuffer().put(data);
+		return bytes.array();
+	}
+
+	public final static byte[] toBytes(double[] data) {
+		ByteBuffer bytes = ByteBuffer.allocate(data.length * Double.BYTES);
+		bytes.asDoubleBuffer().put(data);
+		return bytes.array();
+	}
+}
---
+++ src/com/trollworks/toolkit/utility/Hash.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 1998-2016 by Richard A. Wilkes. All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * version 2.0. If a copy of the MPL was not distributed with this file, You
+ * can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This Source Code Form is "Incompatible With Secondary Licenses", as defined
+ * by the Mozilla Public License, version 2.0.
+ */
+
+package com.trollworks.toolkit.utility;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public final class Hash {
+	public final static byte[] md5(byte[] data) {
+		return hash("MD5", data); //$NON-NLS-1$
+	}
+
+	public final static byte[] sha1(byte[] data) {
+		return hash("SHA-1", data); //$NON-NLS-1$
+	}
+
+	public final static byte[] hash(String algorithm, byte[] data) {
+		try {
+			return MessageDigest.getInstance(algorithm).digest(data);
+		} catch (NoSuchAlgorithmException exception) {
+			throw new IllegalArgumentException(exception);
+		}
+	}
+}
