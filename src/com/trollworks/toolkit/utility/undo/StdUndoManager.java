43177786ff7299c7a1ff7cd088247a1fb5623c85
Richard Wilkes <wilkes@me.com>
2019-01-27 13:14:12 -0800

    Add helper for posting undos

--- src/com/trollworks/toolkit/utility/undo/StdUndoManager.java
+++ src/com/trollworks/toolkit/utility/undo/StdUndoManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1998-2017 by Richard A. Wilkes. All rights reserved.
+ * Copyright (c) 1998-2019 by Richard A. Wilkes. All rights reserved.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, version 2.0. If a copy of the MPL was not distributed with
@@ -11,9 +11,15 @@
 
 package com.trollworks.toolkit.utility.undo;
 
+import com.trollworks.toolkit.ui.UIUtilities;
+import com.trollworks.toolkit.ui.menu.edit.Undoable;
+
+import java.awt.Component;
+
 import javax.swing.undo.CannotRedoException;
 import javax.swing.undo.CannotUndoException;
 import javax.swing.undo.UndoManager;
+import javax.swing.undo.UndoableEdit;
 
 /** The standard {@link UndoManager} for use with our app's windows. */
 public class StdUndoManager extends UndoManager {
@@ -36,5 +42,20 @@ public class StdUndoManager extends UndoManager {
     /** @return Whether this {@link UndoManager} is currently processing an undo or redo. */
     public boolean isInTransaction() {
         return mInTransaction;
+    }
+
+    /**
+     * Post an undoable edit to the undo manager responsible for the component.
+     *
+     * @param comp The component.
+     * @param edit The undoable edit.
+     */
+    public static final void post(Component comp, UndoableEdit edit) {
+        if (comp != null) {
+            Undoable undoable = UIUtilities.getAncestorOfType(comp, Undoable.class);
+            if (undoable != null) {
+                undoable.getUndoManager().addEdit(edit);
+            }
+        }
     }
 }
